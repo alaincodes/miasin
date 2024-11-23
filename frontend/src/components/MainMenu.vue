26a398941ec66c672cbd4ac3097e0a00ff97e55a
Alain TRAN <alain.tran@opsone.net>
2024-11-23 21:28:14 +0100

    add new navbar (wip)

--- frontend/src/components/MainMenu.vue
+++ frontend/src/components/MainMenu.vue
@@ -3,31 +3,37 @@ import { RouterLink } from 'vue-router'
 </script>
 
 <template>
-  <div class="navbar px-0 bg-base-100">
-    <div class="navbar-start">
-      <div class="dropdown">
-        <div tabindex="0" role="button" class="btn btn-ghost lg:hidden">
-          <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" />
-          </svg>
-        </div>
-        <ul tabindex="0" class="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
-          <li>
-            <RouterLink to="/users">Users</RouterLink>
-          </li>
-        </ul>
-      </div>
+  <div class="navbar bg-base-100">
+    <div class="flex-1">
       <RouterLink to="/" class="btn btn-ghost text-xl">MIASIN</RouterLink>
     </div>
-    <div class="navbar-center hidden lg:flex">
-      <ul class="menu menu-horizontal px-1">
-        <li>
-          <RouterLink to="/users">Users</RouterLink>
-        </li>
-      </ul>
-    </div>
-    <div class="navbar-end">
-      <RouterLink to="/subscription" class="btn btn-secondary">Sign up</RouterLink>
+    <div class="flex-none gap-2">
+      <div class="form-control">
+        <input type="text" placeholder="Search" class="input input-bordered w-24 md:w-auto" />
+      </div>
+      <div class="dropdown dropdown-end">
+        <div tabindex="0" role="button" class="btn btn-ghost btn-circle avatar">
+          <div class="w-10 rounded-full">
+            <img
+              alt="Tailwind CSS Navbar component"
+              src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
+          </div>
+        </div>
+        <ul
+          tabindex="0"
+          class="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
+          <li>
+            <a class="justify-between">
+              Profile
+              <span class="badge">New</span>
+            </a>
+          </li>
+          <li><RouterLink to="/subscription">Ajouter un client</RouterLink></li>
+          <li><a>Clients</a></li>
+          <li><a>Settings</a></li>
+          <li><a>Logout</a></li>
+        </ul>
+      </div>
     </div>
   </div>
 </template>
