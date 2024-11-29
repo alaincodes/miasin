18358a9504efcd29c38cc9b307c8a2dd85624120
Alain TRAN <alain.tran@opsone.net>
2024-11-29 23:22:33 +0100

    random test

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  nitro: {
+    prerender: {
+      routes: ["/", "/about"],
+      ignore: ["/blog", "/blog/*"]
+    }
+  },
+  routeRules: {
+    '/legals':{ ssr:false }
+  },
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
   css: ['~/assets/css/main.css'],
