ce1ed1dc883b950ea6158239791aebb402f8f5be
Alain TRAN <alain.tran@opsone.net>
2024-12-19 10:31:42 +0100

    don rememba

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,43 @@
-import "./App.css";
+import "./App.css"
+import { useState } from "react"
+
 
 function App() {
+  const [person, setPerson] = useState({
+    name: "",
+    lastname: "",
+  })
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target
+
+    setPerson((prevState) => ({
+      ...prevState,
+      [name]: value
+    }))
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("Submitted person:", person);
+  }
+
 	return (
 		<>
-			<h1 className="text-3xl font-bold underline">Hello world!</h1>
+			<h1 className="text-3xl font-bold underline">hello world</h1>
+
+      <div>
+        <p>Name: {person.name}</p>
+        <p>Lastname: {person.lastname}</p>
+      </div>
+
+      <form onSubmit={handleSubmit}>
+        <input type="text" name="name" value={person.name} onChange={handleInputChange} />
+        <input type="text" name="lastname" value={person.lastname} onChange={handleInputChange} />
+        <button>submit</button>
+      </form>
 		</>
-	);
+	)
 }
 
-export default App;
+export default App
