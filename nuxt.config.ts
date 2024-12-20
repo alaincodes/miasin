154da83b747ae79eb35386035522dc43b914fc79
Alain TRAN <alain.tran@opsone.net>
2024-12-20 16:43:04 +0100

    add page transition

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-12-20',
+  app: {
+    pageTransition: { name: 'page', mode: 'out-in' }
+  },
   devtools: { enabled: true },
   css: ['~/assets/css/main.css'],
   postcss: {
