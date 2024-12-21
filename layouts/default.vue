20aa6e432756fe6be272beb7334905f14a43b3ed
Alain TRAN <alain.tran@opsone.net>
2024-12-21 23:25:32 +0100

    update header height

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,7 @@
 <template>
   <AppHeader />
 
-  <div class="[--header-height:3.4rem] container grid mx-auto pt-[--header-height] md:min-h-dvh">
+  <div class="[--header-height:3.75rem] container grid mx-auto pt-[--header-height] md:min-h-dvh">
     <slot></slot>
   </div>
 </template>
