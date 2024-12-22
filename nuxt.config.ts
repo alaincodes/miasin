ee1c34d7ded4975f745148dad2198fc60e60a722
Alain Tran <alaint77@gmail.com>
2024-12-22 18:04:23 +0100

    add pinia in modules

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,20 +1,20 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-12-20',
+	compatibilityDate: "2024-12-20",
 
-  app: {
-    pageTransition: { name: 'page', mode: 'out-in' }
-  },
+	app: {
+		pageTransition: { name: "page", mode: "out-in" },
+	},
 
-  devtools: { enabled: true },
-  css: ['~/assets/css/main.css'],
+	devtools: { enabled: true },
+	css: ["~/assets/css/main.css"],
 
-  postcss: {
-    plugins: {
-      tailwindcss: {},
-      autoprefixer: {},
-    },
-  },
+	postcss: {
+		plugins: {
+			tailwindcss: {},
+			autoprefixer: {},
+		},
+	},
 
-  modules: ['@pinia/nuxt', '@nuxt/image'],
-})
+	modules: ["@pinia/nuxt", "@nuxt/image", "@pinia/nuxt"],
+});
