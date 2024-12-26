0b26735bc1c3f3e67cb014c45d517e45705573df
Alain TRAN <alain.tran@opsone.net>
2024-12-26 11:23:45 +0100

    add swiper for cases

--- frontend/components/Cases.vue
+++ frontend/components/Cases.vue
@@ -1,90 +1,112 @@
 <script setup>
+import { onMounted } from "vue";
+import Swiper from 'swiper';
+import { FreeMode } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/free-mode';
 import { useFakeContentStore } from "../stores/useFakeContentStore.js";
 
 const fakeContentStore = useFakeContentStore();
+
+onMounted(() => {
+  new Swiper('.swiper', {
+    modules: [FreeMode],
+    direction: 'vertical',
+    navigation: true,
+    slidesPerView: 1,
+    breakpoints: {
+      768: {
+        freeMode: {
+          enabled: true,
+          sticky: false,
+        },
+        slidesPerView: 'auto',
+      }
+    },
+  });
+});
 </script>
 
 <template>
-	<div
-		class="grid grid-cols-1 gap-6 content-start md:h-full md:overflow-y-auto"
-	>
-		<ul class="flex flex-col divide-y divide-c-yellow-1">
-			<li
-				v-for="item in fakeContentStore.cases"
-				:key="item.id"
-				@click="fakeContentStore.selectCase(item)"
-			>
-				<div
-					class="grid gap-3 max-w-full w-full py-4 rounded-sm duration-300 cursor-pointer md:hover:bg-slate-500/20 md:px-6"
-				>
-					<h2
-						v-if="item.id === 1"
-						class="whitespace-nowrap font-bold uppercase"
-					>
-						Case of the day
-					</h2>
-					<div v-if="item.topics.length > 0" class="flex gap-3">
-						<p>Topic:</p>
-						<ul class="flex flex-wrap gap-1">
-							<li v-for="topic in item.topics">
-								<button
-									class="px-3 py-1 bg-c-yellow-1 rounded-3xl text-c-black text-xs"
-								>
-									{{ topic }}
-								</button>
-							</li>
-						</ul>
-					</div>
-					<div class="flex items-center gap-1">
-						<p class="text-xs italic">
-							<strong>{{ item.accuser }}</strong> filed a complaint against
-							<strong>{{ item.accused }}</strong>
-							on <strong>{{ item.date }}</strong> at
-							<strong>{{ item.time }}</strong>
-						</p>
-					</div>
-					<p class="text-xl">{{ item.message }}</p>
-					<ApexChart
-						:id="item.id"
-						:guiltyCount="item.guiltyCount"
-						:innocentCount="item.innocentCount"
-					/>
-					<p>
-						<strong>{{ item.accused }}</strong> is
-						<span
-							class="font-bold uppercase"
-							:class="{
-								'text-c-yellow-1': item.guiltyCount > item.innocentCount,
-								'text-c-green-3': item.guiltyCount <= item.innocentCount,
-							}"
-						>
-							{{
-								item.guiltyCount > item.innocentCount
-									? "GUILTY !!!"
-									: "INNOCENT !!!"
-							}}
-						</span>
-					</p>
-					<ul class="flex gap-4">
-						<li>
-							<button
-								@click="fakeContentStore.incrementGuilty(item.id)"
-								class="col-span-1 grid grid-flow-col place-items-center gap-2 py-2 px-4 border border-transparent rounded-xl duration-300 bg-c-yellow-1 text-c-black hover:bg-yellow-900 hover:border-yellow-900"
-							>
-								Guilty
-							</button>
-						</li>
-						<li>
-							<button
-								@click="fakeContentStore.incrementInnocent(item.id)"
-								class="col-span-1 grid grid-flow-col place-items-center gap-2 py-2 px-4 border border-transparent rounded-xl duration-300 bg-c-green-3 text-c-black hover:bg-green-900 hover:border-green-900"
-							>
-								Innocent
-							</button>
-						</li>
-					</ul>
-				</div>
-			</li>
-		</ul>
-	</div>
+  <div class="swiper w-full h-full max-h-screen">
+    <div class="swiper-wrapper">
+      <div
+        v-for="item in fakeContentStore.cases"
+        :key="item.id"
+        @click="fakeContentStore.selectCase(item)"
+        class="swiper-slide md:!h-auto"
+      >
+        <div
+          class="grid gap-3 py-4 rounded-sm duration-300 cursor-pointer md:hover:bg-slate-500/20 md:px-6"
+        >
+          <h2
+            v-if="item.id === 1"
+            class="whitespace-nowrap font-bold uppercase"
+          >
+            Case of the day
+          </h2>
+          <div v-if="item.topics.length > 0" class="flex gap-3">
+            <p>Topic:</p>
+            <ul class="flex flex-wrap gap-1">
+              <li v-for="topic in item.topics">
+                <button
+                  class="px-3 py-1 bg-c-yellow-1 rounded-3xl text-c-black text-xs"
+                >
+                  {{ topic }}
+                </button>
+              </li>
+            </ul>
+          </div>
+          <div class="flex items-center gap-1">
+            <p class="text-xs italic">
+              <strong>{{ item.accuser }}</strong> filed a complaint against
+              <strong>{{ item.accused }}</strong>
+              on <strong>{{ item.date }}</strong> at
+              <strong>{{ item.time }}</strong>
+            </p>
+          </div>
+          <p class="text-xl">{{ item.message }}</p>
+          <ApexChart
+            :id="item.id"
+            :guiltyCount="item.guiltyCount"
+            :innocentCount="item.innocentCount"
+          />
+          <p>
+            <strong>{{ item.accused }}</strong> is
+            <span
+              class="font-bold uppercase"
+              :class="{
+                'text-c-yellow-1': item.guiltyCount > item.innocentCount,
+                'text-c-green-3': item.guiltyCount <= item.innocentCount,
+              }"
+            >
+              {{
+                item.guiltyCount > item.innocentCount
+                  ? "GUILTY !!!"
+                  : "INNOCENT !!!"
+              }}
+            </span>
+          </p>
+          <ul class="flex gap-4">
+            <li>
+              <button
+                @click="fakeContentStore.incrementGuilty(item.id)"
+                class="col-span-1 grid grid-flow-col place-items-center gap-2 py-2 px-4 border border-transparent rounded-xl duration-300 bg-c-yellow-1 text-c-black hover:bg-yellow-900 hover:border-yellow-900"
+              >
+                Guilty
+              </button>
+            </li>
+            <li>
+              <button
+                @click="fakeContentStore.incrementInnocent(item.id)"
+                class="col-span-1 grid grid-flow-col place-items-center gap-2 py-2 px-4 border border-transparent rounded-xl duration-300 bg-c-green-3 text-c-black hover:bg-green-900 hover:border-green-900"
+              >
+                Innocent
+              </button>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
