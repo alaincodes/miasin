8c428267ff4cd20444326138a78826c9156a27dd
Alain TRAN <alain.tran@opsone.net>
2024-12-26 16:27:57 +0100

    add hover effect on header links

--- frontend/components/AppHeader.vue
+++ frontend/components/AppHeader.vue
@@ -3,15 +3,14 @@
 		class="z-50 fixed top-0 w-full h-[--header-height] p-4 bg-c-black-light text-white"
 	>
 		<div class="container flex items-center justify-between mx-auto">
-			<NuxtLink href="/" class="grid grid-flow-col items-center gap-2">
+			<NuxtLink href="/" class="grid grid-flow-col items-center gap-2 duration-300 hover:text-c-yellow-1">
         <NuxtImg width="28" height="28" src="/judge.png" densities="x1 x2" />
-        <span class="font-bold hover:text-gradient">MIASIN</span>
+        <span class="font-bold">MIASIN</span>
 			</NuxtLink>
 			<nav>
 				<ul class="flex gap-4">
-					<li><NuxtLink to="/cases/new" class="hover:text-gradient">New Case</NuxtLink></li>
-					<!-- <li class="hidden md:block"><NuxtLink to="/cases">Cases</NuxtLink></li> -->
-					<li><NuxtLink to="/rules" class="hover:text-gradient">Rules</NuxtLink></li>
+					<li><NuxtLink to="/cases/new" class="duration-300 hover:text-c-yellow-1">New Case</NuxtLink></li>
+					<li><NuxtLink to="/rules" class="duration-300 hover:text-c-yellow-1">Rules</NuxtLink></li>
 				</ul>
 			</nav>
 		</div>
