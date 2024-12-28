2a4f8ad7ef48dddc04f5e885dcc303dc8ee6f964
Alain Tran <alaint77@gmail.com>
2024-12-28 17:00:46 +0100

    clean buttons UI

--- frontend/components/Question.vue
+++ frontend/components/Question.vue
@@ -1,14 +1,16 @@
 <script setup>
 defineProps({
-  question: {
-    type: String,
-    required: true,
-  }
-})
+	question: {
+		type: String,
+		required: true,
+	},
+});
 </script>
 
 <template>
-  <div class="prose p-3 rounded-md bg-c-black-light text-white prose-p:leading-6">
-    <p>{{ question }}</p>
-  </div>
+	<div
+		class="prose max-w-full p-3 rounded-md bg-c-black-light text-white prose-p:leading-6"
+	>
+		<p>{{ question }}</p>
+	</div>
 </template>
