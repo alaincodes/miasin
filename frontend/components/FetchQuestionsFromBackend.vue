9afa5320d957117849188fcefa64fa11e5ae4135
Alain TRAN <alain.tran@opsone.net>
2025-01-02 15:45:08 +0100

    add dayjs feat

--- frontend/components/FetchQuestionsFromBackend.vue
+++ frontend/components/FetchQuestionsFromBackend.vue
@@ -1,14 +1,20 @@
 <script setup>
+import dayjs from 'dayjs'
+import relativeTime from 'dayjs/plugin/relativeTime'
+
+dayjs.extend(relativeTime)
 const config = useRuntimeConfig()
 const { data, pending, error } = await useFetch(`${config.public.API_URL}/api/question`)
+
 </script>
 
 <template>
   <div v-if="pending">Chargement...</div>
   <div v-else-if="error">Une erreur est survenue : {{ error.message }}</div>
   <div v-else>
-    <div v-for="question in data" :key="question.id">
+    <div :id="question.id" v-for="question in data" :key="question.id">
       {{ question.content }}
+      {{ dayjs().to(question.createdAt) }}
     </div>
   </div>
 </template>
