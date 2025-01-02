090d881408bd672910aac69b1c7f3364dbc5aa85
Alain TRAN <alain.tran@opsone.net>
2025-01-02 15:45:13 +0100

    add dayjs

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,6 +1,6 @@
 <template>
 	<div class="grid grid-cols-1 p-0 w-full h-[calc(100dvh_-_var(--header-height))] md:h-auto md:justify-self-center xl:w-6/12">
-    <!-- <FetchQuestionsFromBackend /> -->
+    <FetchQuestionsFromBackend />
     <Cases />
 	</div>
 </template>
