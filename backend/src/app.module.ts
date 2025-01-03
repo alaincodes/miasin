594d941e91fd9500b8e72c294e9d12312e6d4397
Alain TRAN <alain.tran@opsone.net>
2025-01-03 11:08:52 +0100

    setup nest config plugin

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { BookmarkModule } from './bookmark/bookmark.module';
@@ -7,6 +8,9 @@ import { QuestionModule } from './question/question.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
     AuthModule,
     UserModule,
     BookmarkModule,
