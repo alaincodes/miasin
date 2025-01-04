97f88f331cd0b8d36a3c372e8398032caf800098
Alain Tran <alaint77@gmail.com>
2025-01-04 11:01:20 +0100

    add fetchquestions component TEST

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,6 +1,8 @@
 <template>
-	<div class="grid grid-cols-1 p-0 w-full h-[calc(100dvh_-_var(--header-height))] md:h-auto md:justify-self-center xl:w-6/12">
-    <!-- <FetchQuestionsFromBackend /> -->
-    <Cases />
+	<div
+		class="grid grid-cols-1 p-0 w-full h-[calc(100dvh_-_var(--header-height))] md:h-auto md:justify-self-center xl:w-6/12"
+	>
+		<FetchQuestionsFromBackend />
+		<Cases />
 	</div>
 </template>
