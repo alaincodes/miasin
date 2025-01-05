96e4b3d43d873e47b28b84730d783219d12570af
Alain Tran <alaint77@gmail.com>
2025-01-05 18:40:31 +0100

    clean question in show page

--- frontend/pages/questions/[id].vue
+++ frontend/pages/questions/[id].vue
@@ -1,18 +1,25 @@
 <script setup>
-import dayjs from 'dayjs'
-import relativeTime from 'dayjs/plugin/relativeTime'
+import dayjs from "dayjs";
+import relativeTime from "dayjs/plugin/relativeTime";
 
-dayjs.extend(relativeTime)
+dayjs.extend(relativeTime);
 
-const route = useRoute()
-const questionId = route.params.id
-const config = useRuntimeConfig()
-const { data } = await useFetch(`${config.public.API_URL}/api/question/${questionId}`)
+const route = useRoute();
+const questionId = route.params.id;
+const config = useRuntimeConfig();
+const { data } = await useFetch(
+	`${config.public.API_URL}/api/question/${questionId}`
+);
 </script>
 
 <template>
-  <div>
-    <p class="italic"><strong>Anonymous</strong> filed a complaint against <strong>Accused</strong> {{ dayjs().to(dayjs(data.createdAt)) }}</p>
-    <Question :question="data.content" />
-  </div>
+	<div
+		class="grid gap-4 h-full content-center mx-auto p-4 md:px-6 md:py-9 md:w-6/12"
+	>
+		<p class="italic">
+			<strong>Anonymous</strong> filed a complaint against
+			<strong>Accused</strong> {{ dayjs().to(dayjs(data.createdAt)) }}
+		</p>
+		<Question :question="data.content" />
+	</div>
 </template>
