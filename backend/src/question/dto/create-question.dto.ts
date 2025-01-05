b0843f9974f52ec03c547756951957b5a5b2ebe0
Alain Tran <alaint77@gmail.com>
2025-01-05 14:27:41 +0100

    add validator for content

--- backend/src/question/dto/create-question.dto.ts
+++ backend/src/question/dto/create-question.dto.ts
@@ -1,7 +1,8 @@
-import { IsNotEmpty, IsString } from 'class-validator';
+import { IsNotEmpty, IsString, Length } from 'class-validator';
 
 export class CreateQuestionDto {
   @IsNotEmpty()
   @IsString()
+  @Length(1, 280)
   content: string;
 }
