1af23845d869bc2342e2c97d31fa9f0c238637b0
Alain Tran <alaint77@gmail.com>
2025-01-13 20:54:30 +0100

    add throttler and helmet for security

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,9 +4,16 @@ import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { QuestionModule } from './question/question.module';
+import { ThrottlerModule } from '@nestjs/throttler';
 
 @Module({
   imports: [
+    ThrottlerModule.forRoot([
+      {
+        ttl: 60000,
+        limit: 10,
+      },
+    ]),
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath: '.env',
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,19 +1,28 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { ThrottlerGuard } from '@nestjs/throttler';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use(helmet());
+
+  app.useGlobalGuards(app.get(ThrottlerGuard));
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
     }),
   );
+
   app.enableCors({
     origin: process.env.FRONTEND_URL || 'http://localhost:3000',
     methods: 'GET,POST,PUT,DELETE,PATCH',
     credentials: true,
   });
+
   await app.listen(process.env.PORT ?? 3333);
 }
 bootstrap();
